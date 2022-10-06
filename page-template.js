2c8dbe35401ab9ea14f16747ff74ca8fa0b896ed
SRoss <srossouw@uwm.edu>
2022-10-05 20:16:27 -0500

    added styling

--- page-template.js
+++ page-template.js
@@ -1,19 +1,18 @@
 const generateManager = managerData => {
     return `
-    <div class="card-deck">
     ${managerData
         .filter(({ officeNumber }) => officeNumber)
         .map(({ name, id, email, role, officeNumber }) => {
         return `
-        <div class="card" style="max-width: 18rem;">
-        <div class="card-body">
-        <h5 class="card-title">Manager</h5>
-        <p>Name: ${name}</p>
-        <p>ID#: ${id}</p>
-        <p>Email Address: <a href="mailto:${email}">${email}</a></p>
-        <p>Role: ${role}</p>
-        <p>Manages Office: ${officeNumber} </p>
-        </div>
+        <div class="card" style="width:400px">
+            <div class="card-body">
+                <div class="card-header lead font-weight-bold">Manager</div>
+                <p>Name: ${name}</p>
+                <p>ID#: ${id}</p>
+                <p>Email Address: <a href="mailto:${email}">${email}</a></p>
+                <p>Role: ${role}</p>
+                <p>Manages Office: ${officeNumber} </p>
+            </div>
         </div>
         `
     })}
@@ -22,47 +21,43 @@ const generateManager = managerData => {
 
 const generateEngineer = engineerData => {
     return `
-    <div class="card-deck">
     ${engineerData
         .filter(({ github }) => github)
         .map(({ name, id, email, role, github }) => {
         return `
-        <div class="card" style="max-width: 18rem;">
-        <div class="card-body">
-        <h5 class="card-title">Engineer</h5>
-        <p>Name: ${name}</p>
-        <p>ID#: ${id}</p>
-        <p>Email Address: <a href="mailto:${email}"> ${email}</a></p>
-        <p>Role: ${role}</p>
-        <p>GitHub Profile: <a href="https:github.com/${github}" target="_blank">GitHub</a></p>
-        </div>
+        <div class="card" style="width:400px">
+            <div class="card-body">
+                <div class="card-header lead font-weight-bold">Engineer</div>
+                <p>Name: ${name}</p>
+                <p>ID#: ${id}</p>
+                <p>Email Address: <a href="mailto:${email}"> ${email}</a></p>
+                <p>Role: ${role}</p>
+                <p>GitHub Profile: <a href="https:github.com/${github}" target="_blank">GitHub</a></p>
+            </div>
         </div>
         `;
     }).join('')}
-    </div>
     `;
 };
 
 const generateIntern = internData => {
     return `
-    <div class="card-deck">
     ${internData
         .filter(({ school }) => school)
         .map(({ name, id, email, role, school }) => {
         return `
-        <div class="card" style="max-width: 18rem;">
-        <div class="card-body">
-        <h5 class="card-title">Intern</h5>
-        <p>Name: ${name}</p>
-        <p>ID#: ${id}</p>
-        <p>Email Address: <a href="mailto:${email}">${email}</a></p>
-        <p>Role: ${role}</p>
-        <p>School: ${school}</p>
-        </div>
-        </div>
+        <div class="card" style="width:400px">
+            <div class="card-body">
+                <div class="card-header lead font-weight-bold">Intern</div>
+                <p>Name: ${name}</p>
+                <p>ID#: ${id}</p>
+                <p>Email Address: <a href="mailto:${email}">${email}</a></p>
+                <p>Role: ${role}</p>
+                <p>School: ${school}</p>
+            </div>
+        </div>   
         `;
     }).join('')}
-    </div>
     `;
 };
 
@@ -75,6 +70,7 @@ module.exports = employeeData => {
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="../assets/style.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css">
         <title>My Team</title>
     </head>
@@ -83,13 +79,13 @@ module.exports = employeeData => {
             <h1 class="text-center">My Team</h1>
         </header>
             <main>
-    
+            <div class="card-deck">
                 ${generateManager(employeeData)}
 
                 ${generateEngineer(employeeData)}
                 
                 ${generateIntern(employeeData)}
-                
+            </div>
             </main>
     </body>
     </html>
